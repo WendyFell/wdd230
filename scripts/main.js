201b8fede7a1d8ce7a5cdb9ac49496ccd6c7f90f
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-09-24 06:50:29 -0600

    Images fixed to correct size. Added link to footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,20 @@
 // Getting the current year
 const date1 = document.querySelector("#date1");
 
-try {
-	const options = {		
-		year: "numeric"
-	};
-	date1.innerHTML = `&copy; ${new Date().toLocaleDateString("en-US", options)}  | Wendy Fellows | Utah, USA`;
-} catch (e) {
-	alert("Error with code or your browser does not support Locale");
-}
+const options = {		
+	year: "numeric"
+};
+date1.innerHTML = `${new Date().toLocaleDateString("en-US", options)}`;
 
 // Getting the last modified date
 const date2 = document.querySelector("#date2");
 
-try {
-	const options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		year: "numeric"
-	};
-	date2.innerHTML = `Last updated ${new Date(document.lastModified).toLocaleDateString("en-US", options)}`;
-} catch (e) {
-	alert("Error with code or your browser does not support Locale");
-}
+const options1 = {
+	weekday: "long",
+	day: "numeric",
+	month: "long",
+	year: "numeric"
+};
+date2.innerHTML = `${new Date(document.lastModified).toLocaleDateString("en-US", options1)}`;
 
 
