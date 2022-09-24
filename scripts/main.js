b443057fbb6184005362dde480a57b2599f92562
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-09-24 07:10:27 -0600

    update js dates

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,9 @@
 // Getting the current year
 const date1 = document.querySelector("#date1");
-
-const options = {		
-	year: "numeric"
-};
-date1.innerHTML = `${new Date().toLocaleDateString("en-US", options)}`;
+date1.innerHTML = new Date().getFullYear();
 
 // Getting the last modified date
 const date2 = document.querySelector("#date2");
-
-const options1 = {
-	weekday: "long",
-	day: "numeric",
-	month: "long",
-	year: "numeric"
-};
-date2.innerHTML = `${new Date(document.lastModified).toLocaleDateString("en-US", options1)}`;
+date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
 
 
