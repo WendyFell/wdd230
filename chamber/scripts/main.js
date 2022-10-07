ca1bbaba9dd406d57d985205452f8e8a6d964bf8
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-10-06 18:52:11 -0600

    fixed file name and grid header

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,1 +1,25 @@
+// Getting a full date
+const datefield = document.querySelector("#date"); // Select the element to manipulate
 
+const now = new Date(); // derive the current date using a date object
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+datefield.innerHTML = fulldate;
+
+
+// Getting the current year
+const date1 = document.querySelector("#year");
+date1.innerHTML = new Date().getFullYear();
+
+// Getting the last modified date
+const date2 = document.querySelector("#date2");
+date2.innerHTML = new Date(document.lastModified).toLocaleDateString("en-US");
+
+// Toggle menu with hamburger button
+function toggleMenu () {
+    document.getElementById("navigation").classList.toggle("open");
+    document.getElementById("hamBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamBtn");
+x.onclick = toggleMenu;
