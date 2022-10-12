14701aea39f16a166ce1358d0fe83707be25b8c2
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-10-12 06:45:02 -0600

    fixed chamber header to flex added week 5

---
+++ week5/scripts/main.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    // if 
+    const myItem = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li'); // Create an li element
+    const delBtn = document.createElement('button'); // Create a delete button
+    const listText = document.createElement('span'); //Create a span element
+
+    listItem.appendChild(listText); // append the span as child of the list item (li)
+    listText.textContent = myItem; // Populate the li elements textContent or innerHTML with the input
+    
+    listItem.appendChild(delBtn); // append the delete button as child of the list item (li)
+    delBtn.textContent = '❌'; // Populate the button textContent with an X
+        
+    list.appendChild(listItem); // append the list item (li) as a child of the list
+
+    // Add an event listener to the delete button that removes the li element when clicked
+    delBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+
+    input.focus();
+
+});
