7d246561bf3b11ea46c6aa79aede181bbc5fdaaf
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-10-29 06:33:19 -0600

    added images, cleaned up some line code

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -25,22 +25,7 @@ const x = document.getElementById("hamBtn");
 x.onclick = toggleMenu;
 
 
-// // Another method from week 4 by Brother Blazzard
-// const hambutton = document.querySelector('.ham');
-// const mainnav = document.querySelector('.navigation')
 
-// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// // To solve the mid resizing issue with responsive class on
-// window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-// /*** Programming Notes **************************************
-//   Arrow Functions - es6 syntactically compact alternative to a regular function expression
-//   see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-//   or https://www.w3schools.com/js/js_arrow_function.asp
-
-//   classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-//   */
 
 
 // Get the meet and greet button to show only on Monday and Tuesdays
@@ -58,3 +43,11 @@ else {
 }
 
 
+
+
+// active page has aria attribute
+document.querySelectorAll(".navLink").forEach(link => {
+    if(link.href === window.location.href){
+        link.setAttribute("aria-current", "page")
+    }
+});
