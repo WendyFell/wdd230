fec3e0133633ab1574d6d257786fd69a04cd15e1
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-11-26 17:06:42 -0700

    fixing things from validation

--- chamber/scripts/form.js
+++ chamber/scripts/form.js
@@ -1,25 +1,3 @@
-// Form
-// const form = document.querySelector('#form');
-// const input1 = document.querySelector('#phone');
-// const input2 = document.querySelector("#email");
-// const input3= document.querySelector("#title");
-// const output = document.querySelector('#output');
-
-// const regex1 = /^(?:\d{3}|\(\d{3}\))([-/.])\d{3}\1\d{4}$/;
-// const regex2 = [A-Z][a-z];
-
-// function testInfo(phoneInput) {
-//   const ok = regex1.exec(phoneInput.value);
-
-//   output.textContent = ok
-//     ? `Thanks, your phone number is ${ok[0]}`
-//     : `${phoneInput.value} isn't a phone number with area code!`;
-// }
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   testInfo(input1);
-// });
 
 const subTime = document.querySelector("#submissionTime");
 
