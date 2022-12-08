e79d22972b6d132a99ff778122f17e90d9de5c8b
WendyFell <90659020+WendyFell@users.noreply.github.com>
2022-12-07 17:55:52 -0700

    list of fruits works

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -1,8 +1,29 @@
 const fruitURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
-const myFruitList = json.parse(fruitURL);
-console.log(myFruitList);
-x = myFruitList.name;
-console.log(x);
+
+const getTemples = async () => {
+  const response = await fetch(
+    "https://brotherblazzard.github.io/canvas-content/fruit.json"
+  );
+  templeList = await response.json();
+  output(templeList);
+};
+getTemples();
+
+const output = (temples) => {
+  temples.forEach((temple) => {
+    let article = document.createElement("article");
+
+    let templeName = document.createElement("h3");
+    templeName.textContent = temple.name;
+
+   
+
+    article.appendChild(templeName);
+   
+
+    document.querySelector("#myData").appendChild(article);
+  });
+};
 
 
 
@@ -28,51 +49,6 @@ console.log(x);
 // function getFruitList(data) {
 //   console.log(data);
 //   results = data;
- 
+
 
 // }
-
-// const url = "https://pokeapi.co/api/v2/pokemon/ditto";
-// const urlList = "https://pokeapi.co/api/v2/pokemon";
-// let results = null;
-
-// async function getPokemon(url, doThis) {
-//   const response = await fetch(url);
-//   //check to see if the fetch was successful
-//   if (response.ok) {
-//     // the API will send us JSON...but we have to convert the response before we can use it
-//     // .json() also returns a promise...so we await it as well.
-//     const data = await response.json();
-//     // execute the callback
-//     doThis(data);
-//   }
-// }
-
-// function doStuff(data) {
-//   results = data;
-//   const outputElement = document.querySelector("#output");
-//   const html = `<h2>${data.name}</h2><img src="${data.sprites.front_default}" alt="${data.name}">`;
-//   outputElement.innerHTML = html;
-//   console.log("first: ", results);
-// }
-
-// function doStuffList(data) {
-//   console.log(data);
-//   const pokeListElement = document.querySelector("#outputList");
-//   const pokeList = data.results;
-//   pokeList.forEach((currentItem) => {
-//     const html = `<li data-url="${item.url}">${currentItem.name}</li>`;
-//     // note the += here...
-//     pokeListElement.innerHTML += html;
-//   });
-// }
-// getPokemon(url, doStuff);
-// console.log("second: ", results);
-// // Notice that by just passing a different callback function in
-// // we can totally change what happens when the data comes back.
-// // It's like we gave the getPokemon function superpowers!
-// getPokemon(urlList, doStuffList);
-
-
-
-
